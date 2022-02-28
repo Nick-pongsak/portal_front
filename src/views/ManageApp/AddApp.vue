15bdf46848317b3258cd519013d9dd6e488bd14c
T <Tutiyaporn@dhas.com>
2022-02-28 16:46:08 +0700

    no message

--- src/views/ManageApp/AddApp.vue
+++ src/views/ManageApp/AddApp.vue
@@ -836,7 +836,7 @@ export default {
       let value = evt.target.value
       let thai = /[ก-ฮ]/g
       let numThai = /[๑-๙]/g
-      let charac = /[=%฿.-_$~`:;'"!@#^&{}/|+()[\]*\\]/g
+      let charac = /[=%฿.-_$~`:;'"!><@#^&{}/|+()[\]*\\]/g
       let rsChar = value.search(thai)
       let rsNum = value.search(numThai)
       let rsCharac = value.search(charac)
