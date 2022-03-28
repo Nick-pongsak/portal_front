09667ec760b815d47f2495e056780590165ca5be
T <Tutiyaporn@dhas.com>
2022-03-28 17:20:14 +0700

    no message

--- src/views/AdminApp/Main.vue
+++ src/views/AdminApp/Main.vue
@@ -246,7 +246,7 @@ export default {
     }
   },
   methods: {
-    upload_test () {
+    upload () {
       let data = {
         new: [],
         update: this.list,
@@ -256,7 +256,7 @@ export default {
       data.total = data.new.length + data.update.length + data.mistake.length
       this.$emit('upload', data)
     },
-    upload () {
+    upload_test () {
       this.$refs.uploaderCsv.click()
     },
     setImage: function (output) {
