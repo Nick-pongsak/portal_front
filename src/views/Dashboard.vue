267bf4c17b4ad0c57262a53380afb028936557d9
T <Tutiyaporn@dhas.com>
2022-04-25 13:17:00 +0700

    no message

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -428,7 +428,7 @@ export default {
       // if (sciHead2.length < 3) {
       var script = document.createElement('script')
       let str = unescape(
-        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K5BVKGB')"
+        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K79C9J3')"
       )
       // script.src = unescape("(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({'gtm.start':new Date().getTime(), event: 'gtm.js'});var f = d.getElementsByTagName(s)[0],j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src ='https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);})(window, document, 'script', 'dataLayer', 'GTM-PVDSH95')")
       //  console.log(script)
@@ -439,7 +439,7 @@ export default {
 
       // insert body
       let bodyHtml =
-        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5BVKGB"height="0" width="0" style="display:none;visibility:hidden"></iframe>'
+        '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K79C9J3"height="0" width="0" style="display:none;visibility:hidden"></iframe>'
       // let nos = document.getElementsByTagName('noscript')[0]
       var noscript = document.createElement('noscript')
       noscript.innerHTML = bodyHtml
